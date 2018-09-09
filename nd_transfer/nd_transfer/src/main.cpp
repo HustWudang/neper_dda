51f08e9e2c09507ccf94e77e7f7f3332141e1b2a
Fei ZHENG <feizhengprchina@hotmail.com>
2018-09-09 15:54:20 +0800

    An example.

--- nd_transfer/nd_transfer/src/main.cpp
+++ nd_transfer/nd_transfer/src/main.cpp
@@ -1,1 +1,28 @@
+/*
+* Main procedure to make the transfer
+* from Neper file "0.tess"
+* to DDA program "0_block.json"
+*/
+
+// header file.
 #include "structure.h"
+
+// namespace std;
+using namespace std;
+
+// main function.
+int main()
+{
+	// [Screen Reminder].
+	cout << "-----\n";
+	cout << " Transfer Gmsh file (1.msh) to DDA block file (block.json) ...\n";
+	cout << "-----\n";
+
+	NeperDDA mNeperDDA_1;
+	mNeperDDA_1.fReadNeper_tess("0.tess");
+	mNeperDDA_1.fWriteDDABlock_json("0_block.json");
+	mNeperDDA_1.fWriteDDABlock_vtp("0_block.vtp");
+	// [Main function return].
+	system("pause");
+	return 0;
+}
--- nd_transfer/nd_transfer/src/structure.h
+++ nd_transfer/nd_transfer/src/structure.h
@@ -1,1 +1,52 @@
 #pragma once
+
+// lib header.
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <vector>
+
+// namespace std;
+using namespace std;
+
+// Gmsh basic elements.
+class CNeperVertex
+{
+public:
+	int i_No;
+	int i_state;
+	double x, y, z;
+};
+
+class CNeperFace
+{
+public:
+	int i_No;
+	int i_NumVertex;
+	int i_NumEdge;
+	vector<int> lv_VertexNo;
+	vector<int> lv_EdgeNo;
+	double d, a, b, c;
+	int i_FaceState;
+	int i_FacePoint;
+	double fp_x, fp_y, fp_z;
+};
+
+class NeperDDA
+{
+public:
+	int i_NumVertex;
+	int i_NumFace;
+	vector<CNeperVertex> lv_NeperVertex;
+	vector<CNeperFace> lv_NeperFace;
+	void fReadNeper_tess(string str_0);
+	void fWriteDDABlock_json(string str_0);
+	void fWriteDDABlock_vtp(string str_0);
+};
+
+
+
+
