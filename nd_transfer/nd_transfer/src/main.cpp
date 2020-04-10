2778415a57b4bd620f1d6c60664ce8944dd47568
Zheng_Fizz <feizhengprchina@hotmail.com>
2020-04-10 12:57:12 +0200

    file input method is updated.

--- nd_transfer/nd_transfer/src/main.cpp
+++ nd_transfer/nd_transfer/src/main.cpp
@@ -10,6 +10,25 @@
 // namespace std;
 using namespace std;
 
+// function to obtain string (file name path) from "filename.txt".
+// the file name is used to define all file names associated with the analysis module.
+std::string fGetFileNamePathStringFromFileInRootPath(std::string s_RootFileName)
+{
+	// [File-name] Load filename.txt in root directory ...
+	string s_FilePath;
+	ifstream iflname;
+	iflname.open(s_RootFileName);
+	if (iflname.is_open())
+	{
+		getline(iflname, s_FilePath);
+		std::cout << "[Msg][File] open filename.txt ... \n";
+		std::cout << "[Msg] Get filename for model & analyis ... \n";
+		iflname.close();
+	}
+	else { std::cout << "[Error][File] open filename.txt !\n"; }
+	return s_FilePath;
+}
+
 // main function.
 int main()
 {
@@ -17,11 +36,14 @@ int main()
 	cout << "-----\n";
 	cout << " Transfer Gmsh file (1.msh) to DDA block file (block.json) ...\n";
 	cout << "-----\n";
-
+	std::string sfin_base = fGetFileNamePathStringFromFileInRootPath("filename.txt");
+	std::string sfin_tess = sfin_base + ".tess";
+	std::string sfot_blk_json = sfin_base + "_block.json";
+	std::string sfot_blk_vtp = sfin_base + "_block.vtp";
 	NeperDDA mNeperDDA_1;
-	mNeperDDA_1.fReadNeper_tess("1.tess");
-	mNeperDDA_1.fWriteDDABlock_json("1_block.json");
-	mNeperDDA_1.fWriteDDABlock_vtp("1_block.vtp");
+	mNeperDDA_1.fReadNeper_tess(sfin_tess);
+	mNeperDDA_1.fWriteDDABlock_json(sfot_blk_json);
+	mNeperDDA_1.fWriteDDABlock_vtp(sfot_blk_vtp);
 	// [Main function return].
 	system("pause");
 	return 0;
